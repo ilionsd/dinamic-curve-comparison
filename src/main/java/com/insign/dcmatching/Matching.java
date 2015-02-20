56895d6e30c19d0e8ba96e3fdd60999f13fe8462
Semyon Burov <ilionsd@gmail.com>
2015-02-20 12:36:06 +0300

    Initial commit

---
+++ src/main/java/com/insign/dcmatching/Matching.java
@@ -0,0 +1,7 @@
+package com.insign.dcmatching;
+
+/**
+ * Created by ilion on 20.02.2015.
+ */
+public class Matching {
+}
---
+++ src/main/java/com/insign/dcmatching/Signature.java
@@ -0,0 +1,9 @@
+package com.insign.dcmatching;
+
+/**
+ * Created by ilion on 20.02.2015.
+ */
+public class Signature {
+	
+
+}
