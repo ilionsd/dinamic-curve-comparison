f04010bed03eeed2fa6116601fdd18240b449a9c
Semyon Burov <ilionsd@gmail.com>
2015-03-11 12:55:39 +0300

    Added methods, that calculates extremum points of CubicSpline and Curvature function of NaturalSplineParametricCurve

--- src/main/java/com/insign/dccomparison/SignatureUtils.java
+++ src/main/java/com/insign/dccomparison/SignatureUtils.java
@@ -6,6 +6,8 @@ import com.insign.common.function.differentialgeometry.NaturalCubicSplineParamet
 import com.insign.common.function.differentialgeometry.CubicSplineParametricCurve;
 import com.insign.common.function.integration.Integral;
 import com.insign.common.function.integration.Intergrate;
+import com.insign.common.function.interpolation.CubicSpline;
+import com.insign.common.function.interpolation.SplineSegment;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.ArrayList;
@@ -40,8 +42,9 @@ public class SignatureUtils {
 	 * @return Signature instance
 	 */
 	public static Signature createFromCurve(final NaturalCubicSplineParametricCurve curve) {
-		List<Point2D> xsExtremum = new ArrayList<Point2D>();
-		List<Point2D> ysExtremum = new ArrayList<Point2D>();
+		List<Point2D> xsExtremum = getExtremumPoints(curve.getX());
+		List<Point2D> ysExtremum = getExtremumPoints(curve.getY());
+		List<Point2D> csExtremum = getCurvatureExtremumPoints(curve.getX(), curve.getY());
 
 		throw new NotImplementedException();
 	}
@@ -62,10 +65,74 @@ public class SignatureUtils {
 	}
 
 	private LinkedHashMap<Double, Point2D> getCriticalPoints(NaturalParametricCurve curve) {
-
-
-
 		return null;
+	}
+
+	public static List<Point2D> getExtremumPoints(final CubicSpline cubicSpline) {
+		List<Point2D> extremumPoints = new ArrayList<Point2D>();
+		for (int k = 0; k < cubicSpline.segmentCount(); k++) {
+			SplineSegment segment = cubicSpline.get(k);
+			/*
+			f(x)=ax^3+bx^2+cx+d
+			f'(x)=3ax^2+2bx+c
+			 */
+			double[] coefficients = new double[3];
+			for (int index = 1; index <= segment.power(); index++)
+				coefficients[index - 1] = segment.get(index) * index;
+			double discriminant = coefficients[1] * coefficients[1] - 4.0 * coefficients[2] * coefficients[0];
+			List<Double> roots = new ArrayList<Double>();
+			if (Double.compare(Math.abs(discriminant), Double.MIN_VALUE) <= 0) {
+				//-- 1 root --
+				roots.add((-coefficients[1]) / (2 * coefficients[2]));
+			}
+			else if (discriminant > 0) {
+				//-- 2 roots --
+				double discriminantSqrt = Math.sqrt(discriminant);
+				roots.add((-coefficients[1] + discriminantSqrt) / (2 * coefficients[2]));
+				roots.add((-coefficients[1] - discriminantSqrt) / (2 * coefficients[2]));
+			}
+			for (double root : roots) {
+				if (segment.isIn(root)) {
+					Point2D extremum = new Point2D(root, segment.valueIn(root));
+					extremumPoints.add(extremum);
+				}
+			}
+		}
+		return extremumPoints;
+	}
+
+	public static List<Point2D> getCurvatureExtremumPoints(final CubicSpline xs, final CubicSpline ys) {
+		List<Point2D> extremumPoints = new ArrayList<Point2D>();
+		double[] xKnots = xs.getKnots();
+		double[] yKnots = ys.getKnots();
+		double[] overallKnots = com.insign.common.ArrayUtils.union(xKnots, yKnots);
+		for (int k = 1; k < overallKnots.length; k++) {
+			double middle = (overallKnots[k - 1] + overallKnots[k]) / 2.0;
+			int xsIndex = xs.getSegment(middle),
+				ysIndex = ys.getSegment(middle);
+			SplineSegment xsSegment = xs.get(xsIndex),
+						  ysSegment = ys.get(ysIndex);
+			double[] xsCoefficients = new double[2],
+					 ysCoefficients = new double[2];
+			for (int power = 2; power < xsSegment.power(); power++)
+				xsCoefficients[power - 2] = xsSegment.get(power);
+			for (int power = 2; power < ysSegment.power(); power++)
+				ysCoefficients[power - 2] = ysSegment.get(power);
+			double sForbidden = Double.NaN;
+			if (Double.compare(Math.abs(xsCoefficients[0] - ysCoefficients[0]), 1e-6) <= 0 && Double.compare(Math.abs(xsCoefficients[1] - ysCoefficients[1]), 1e-8) <= 0) {
+				sForbidden = xsCoefficients[0] / (3.0 * xsCoefficients[1]);
+			}
+			double a1b1_plus_a2b2 = xsCoefficients[0] * xsCoefficients[1] + ysCoefficients[0] * ysCoefficients[1];
+			double a1a1_plus_a2a2 = xsCoefficients[1] * xsCoefficients[1] + ysCoefficients[1] * ysCoefficients[1];
+			double s = - (24.0 * a1b1_plus_a2b2) / (72.0 * a1a1_plus_a2a2);
+			if (Double.compare(Math.abs(s - sForbidden), 1e-8) > 0 && xsSegment.isIn(s) && ysSegment.isIn(s)) {
+				double b1b1_plus_b2b2 = xsCoefficients[0] * xsCoefficients[0] + ysCoefficients[0] * ysCoefficients[0];
+				double cs = Math.sqrt(36.0 * a1a1_plus_a2a2 * s * s + 24.0 * a1b1_plus_a2b2 * s + 4.0 * b1b1_plus_b2b2);
+				Point2D extremum = new Point2D(s, cs);
+				extremumPoints.add(extremum);
+			}
+		}
+		return extremumPoints;
 	}
 
 
